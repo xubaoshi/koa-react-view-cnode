9033859778c9def1c3585d58aec8278558ff4ccc
xubaoshi <948869516@qq.com>
2016-07-29 15:16:58 +0800

    revert code

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
  * Created by xubaoshi on 2016/7/27.
  */
 var koa = require('koa');
+var react = require('koa-react-view');
+var path = require('path');
+
 var app = koa();
+
+var viewpath = path.join(__dirname,'views');
+var assetspath = path.join(__dirname,'public');
 
 app.use(function *(){
 	this.body = 'Hello World';
 });
+
+react(app,{
+	views:viewpath;
+})
 
 app.listen(3000);
 
